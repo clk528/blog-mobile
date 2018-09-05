7cac62348047247a8e685bf8d2b0ee19adcdbf0c
clk528 <clk@clk528.com>
2018-09-05 17:02:03 +0800

    长太息以掩涕兮，哀民生之多艰

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import Color from "./../../plugin/Colors";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    items: Array<{ items: Array<{ color: string }>, class: string }>;
 
-  }
+    constructor(public navCtrl: NavController) {
+        this.items = [];
+        Color.forEach((item) => {
+            let data = {
+                class: 'div',
+                items: []
+            };
+            item.forEach((color) => {
+                data.items.push({
+                    color: color
+                });
+            });
 
+            this.items.push(data)
+        });
+
+        setTimeout(()=>{
+            this.test();
+        },500);
+    }
+
+    test(){
+        console.log(this.items);
+    }
 }
